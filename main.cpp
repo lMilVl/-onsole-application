36e671ccfb78660d5cf298b1b9abba874800a409
lMilVl <milena.vasiljeva2000@yandex.ru>
2020-03-11 12:00:26 +0500

    Для различных типов ошибок написаны разные коды завершения программы.

--- main.cpp
+++ main.cpp
@@ -8,54 +8,54 @@ using namespace std;
 int main(int argc, char * argv[])
 {
 
-        if(argc < 3) {
+    if(argc < 3) {
         // вывести сообщение об ошибке и завершить работу программы
         std::cerr << "Missing parameters" << std::endl;
         return 1;
-        }
-        int a, b;
+    }
+    int a, b;
 
-        istringstream astr(argv[1]), bstr(argv[2]);
+    istringstream astr(argv[1]), bstr(argv[2]);
 
-        astr >> a;
-        bstr >> b;
-        if(astr.fail() || !astr.eof()) {
+    astr >> a;
+    bstr >> b;
+    if(astr.fail() || !astr.eof()) {
         // вывести сообщение об ошибке и завершить работу программы
         std::cerr << "A must be an integer" << std::endl;
         return 2;
-        }
-        else if (a < 0)
-        {
-            std::cerr << "A must be a positive";
-            return 2;
-        }
+    }
+    if (a < 0)
+    {
+        std::cerr << "A must be a positive";
+        return 3;
+    }
 
 
-        if(bstr.fail() || !bstr.eof()) {
+    if(bstr.fail() || !bstr.eof()) {
         // вывести сообщение об ошибке и завершить работу программы
         std::cerr << "B must be an integer" << std::endl;
         return 2;
-        }
-        else if (b < 0)
-        {
-            std::cerr << "B must be a positive";
-            return 2;
-        }
+    }
+    if (b < 0)
+    {
+        std::cerr << "B must be a positive";
+        return 3;
+    }
 
-        if (a > b)
-        {
-            std:: cerr << "A must be less than B";
-            return 2;
-        }
+    if (a > b)
+    {
+        std:: cerr << "A must be less than B";
+        return 4;
+    }
 
 
-        auto result = ListOfPrimeNumbers(a, b);
-        cout << "Result: ";
+    auto result = ListOfPrimeNumbers(a, b);
+    cout << "Result: ";
 
-        for (auto temp : result)
-        {
-            cout << temp << " ";
-        }
+    for (auto temp : result)
+    {
+        cout << temp << " ";
+    }
 
-        return 0;
+    return 0;
 }
